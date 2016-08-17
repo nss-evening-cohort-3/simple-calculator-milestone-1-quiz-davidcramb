3c9ffc23ac03a068dbc54ed35d262b45bddd74f4
David Cramb <david.cramb@gmail.com>
2016-08-16 22:35:11 -0500

    Progressing on building expression array...

--- SimpleCalculator.Tests/ExpressionsTest.cs
+++ SimpleCalculator.Tests/ExpressionsTest.cs
@@ -13,6 +13,7 @@ namespace SimpleCalculator.Tests
             Expression expressed = new Expression("1+1");
             string expected = "1+1";
             string actual = expressed.expression_as_string;
+
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
@@ -20,18 +21,49 @@ namespace SimpleCalculator.Tests
         {
             Expression expressed = new Expression("1+1");
             char[] expected = {'1', '+', '1'};
-            
             char[] actual = expressed.splitExpressionString(expressed.expression_as_string);
+
             CollectionAssert.AreEquivalent(expected, actual);
         }
         [TestMethod]
         public void ExpressionDoesNotSplit()
         {
+            //Going down a failed path to split the string array
             Expression expressed = new Expression("1+1");
             string expected = "1+1";
             expressed.splitExpressionString(expressed.expression_as_string);
-            string actual = expressed.expression_as_array[0];
+            string actual = expressed.expression_as_array[0]; 
+
             Assert.AreSame(expected, actual);
         }
+        [TestMethod]
+        public void ExpressionCharArrayDisassembledFurther()
+        {
+            Expression expressed = new Expression("10+20");
+            char[] expected = { '1','0', '+', '2','0' };
+            char[] actual = expressed.splitExpressionString(expressed.expression_as_string);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+        [TestMethod]
+        public void ExpressionParseOperandThrowsException()
+        {
+            Expression expressed = new Expression("11+22");
+            expressed.splitExpressionString(expressed.expression_as_string);
+            char[] charArray = { '1', '1', '+', '2', '2' };
+            Int32.Parse(charArray[2]);
+        }
+        [TestMethod]
+        public void ExpressionConverCharArrayTo3StringArray()
+        {
+            Expression expressed = new Expression("11+22");
+            expressed.splitExpressionString(expressed.expression_as_string); //"1+2"
+            string[] expressionArray = { "11", "+", "22" };
+            int expected = 11;
+                        Int32.Parse(expected[0]);
+            Assert.AreEqual(expected[0], 11);
+
+        }
+
     }
 }
--- SimpleCalculator/Expression.cs
+++ SimpleCalculator/Expression.cs
@@ -12,17 +12,43 @@ namespace SimpleCalculator
         {
             expression_as_string = userGivenExpression;
             expression_as_array = new string[3];
-                        
+            expression_as_char_array = expression_as_string.ToCharArray();
+            //LHS = LHS; 
+            //RHS = RHS;
+            operand = operand;
+
         }
         public string expression_as_string { get; set; }
         public string[] expression_as_array { get; set; }
+        public char[] expression_as_char_array { get; set; }
+        public int LHS { get; set; }
+        public int RHS { get; set; }
+        public string operand { get; set; }
 
         //parse the expression as string into LHS#, Operand, and RHS#
-        public string[] splitExpressionString(string expression_as_string)
+
+        //this method may be unnecessary
+        public char[] splitExpressionString(string expression_as_string)
         {
-            string[] testarray = expression_as_string.Split(new char[] { '+' });
-            return expression_as_array = testarray;
-            
+            int number;
+            List<int> testList = new List<int>();
+            char[] expressionCharArray = expression_as_string.ToCharArray();
+            foreach (var character in expressionCharArray)
+            {
+                Int32.TryParse(character.ToString(), out number);
+                testList.Add(number);
+            }
+            return expressionCharArray;
+        }
+
+        public string[] ExpressionAsArray(char[] expression_as_char_array)
+        {
+            int operand = Array.IndexOf(expression_as_char_array, ('+' | '-' | '%' | '/' | '*'));
+            //find index of operand to use as length of for loop, ignore foreach code below
+            foreach(var character in expression_as_char_array)
+            {
+            }
+            //
         }
 
         
