8042970133caabc5e3b98321c589d7748a8e5eef
David Cramb <david.cramb@gmail.com>
2016-08-22 17:13:03 -0500

    added counter

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -12,31 +12,51 @@ namespace SimpleCalculator
         static void Main(string[] args)
         {
             bool keepGoing = true;
+            int counter = 0;
             Regex pemdas = new Regex(@"(\s*)?[\d](\s*)?(\+|\-|\*|\%|\/)(\s*)?[\d]+(\s*)?"); //not sure if i will use this
-            string[] quitArray = { "no", "quit", "stop", "exit", "q" };
+            string[] quitArray = { "no", "quit", "stop", "exit"};
+            Stack lastExpression = new Stack();
             
-            var prompt = ">";
-            Console.WriteLine("Let's Play with Baby's First Calculator");
+            Console.WriteLine("This is a Calculator. There are many like it but this one is mine.");
             //While loop to kee
             while (keepGoing)
             {
+                var prompt = ">";
                 Console.WriteLine("Add, Subtract, Multiply, Divide, or get a Remainder");
+                Console.Write("[{0}] {1} ", counter, prompt);
                 string userPrompt = Console.ReadLine().ToLower();
-                Console.WriteLine(prompt);
-                
+
                 if (quitArray.Any(userPrompt.Contains))
                 {
                     keepGoing = false;
                     Console.WriteLine("Bye!");
                     Console.ReadLine();
                 }
+                else if (userPrompt == "last")
+                {
+                    lastExpression.GetLastAnswer();
+                }
+                else if (userPrompt == "lastq")
+                {
+                    //lastExpression.GetLastExpressionQueried(userPrompt);
+                }
                 else
                 {
                     Expression userExpression = new Expression();
-                    userExpression.ExpressionSplit(userPrompt);
-                    Calculate calculation = new Calculate(userExpression.LHS, userExpression.RHS, userExpression.operand);
-                    calculation.DetermineOperation();
-                    Console.WriteLine(calculation.result);
+                    try
+                    { 
+                        userExpression.ExpressionSplit(userPrompt);
+                        //lastExpression.LastExpressionQueried(userPrompt);
+                        Calculate calculation = new Calculate(userExpression.LHS, userExpression.RHS, userExpression.operand);
+                        calculation.DetermineOperation();
+                        //lastExpression.LastAnswer(calculation.result);
+                        Console.WriteLine(calculation.result);
+                        counter++;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
                 }
             }
         }
