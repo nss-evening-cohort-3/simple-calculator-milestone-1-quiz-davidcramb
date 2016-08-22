49badb98df8c8ba3c3d9f39fa40eb3a23584af0f
David Cramb <david.cramb@gmail.com>
2016-08-21 22:50:39 -0500

    almost finalized Stack class

---
+++ SimpleCalculator.Tests/StackTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Tests
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void StacksSetsAnswer()
+        {
+            Expression expresso = new Expression();
+            Stack stacky = new Stack();
+            expresso.ExpressionSplit("2+3");
+            Calculate cal = new Calculate(expresso.LHS, expresso.RHS, expresso.operand);
+            cal.DetermineOperation();
+            stacky.SetLastAnswer(cal.result);
+            int expected = 5;
+            int actual = stacky.last_answer.Peek();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void StackSetsExpression()
+        {
+            Expression exp = new Expression();
+            Stack stacky = new Stack();
+            exp.ExpressionSplit("5*10");
+            Calculate cal = new Calculate(exp.LHS, exp.RHS, exp.operand);
+            cal.DetermineOperation();
+            stacky.SetLastExpressionQueried(exp.expression_as_string);
+            string expected = "5*10";
+            string actual = stacky.last_expression.Peek();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SimpleCalculator/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Stack
+    {
+        public Stack ()
+        {
+        }
+        public Stack<int> last_answer { get; set; }
+        public Stack<string> last_expression { get; set; }
+
+        public void SetLastAnswer(int result)
+        {
+            last_answer = new Stack<int>();
+            last_answer.Push(result);
+        }
+
+        public int GetLastAnswer()
+        {
+            int answer = last_answer.Pop();
+            return answer;
+        }
+        public void SetLastExpressionQueried(string userExpression)
+        {
+            last_expression = new Stack<string>();
+            last_expression.Push(userExpression);
+        }
+
+    }
+}
